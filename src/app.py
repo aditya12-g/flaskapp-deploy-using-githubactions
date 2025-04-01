cc3220bb57bd26a5c73f30a1951ec378f2f10f16
Aditya Gangthade <adityagangthade1998@gmail.com>
2025-04-01 19:16:09 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_docker():
-    return '<h1> This is Aditya Gangthade</h1><br><p>Thank you for visiting. </p> '
+    return '<h1> This is Aditya Gangthade</h1><br><p>hope you are doing well. </p> '
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
